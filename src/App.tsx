efc3064a846062f75ce1a2350d70749015bb677d
tkerdsakun-bit <tkerdsakun@gmail.com>
2026-01-04 19:20:01 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,10 +24,8 @@ const App = () => {
 
   const [assets, setAssets] = useState<Asset[]>([]);
   const [departments, setDepartments] = useState<Department[]>([]);
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const [inkInventory, setInkInventory] = useState<any[]>([]);
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const [inkBudget, setInkBudget] = useState<any>(null);
+const [, setInkInventory] = useState<InkItem[]>([]);
+const [, setInkBudget] = useState<InkBudgetSummary | null>(null);
   const [assetCategories, setAssetCategories] = useState<AssetCategory[]>([]);
   const [repairHistory, setRepairHistory] = useState<RepairHistory[]>([]);
   const [inkTransactions, setInkTransactions] = useState<InkTransaction[]>([]);
