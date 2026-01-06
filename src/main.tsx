f8a2b3aa071c985c8cb2419a8b304e572e259c97
tkerdsakun-bit <tkerdsakun@gmail.com>
2026-01-06 08:50:34 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import AssetDetail from './AssetDetail'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/asset/:assetId" element={<AssetDetail />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
