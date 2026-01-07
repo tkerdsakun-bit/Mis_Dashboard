33220b8e9171dcca6a1f44c0267611099b595d08
tkerdsakun-bit <tkerdsakun@gmail.com>
2026-01-07 20:13:39 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+import App from './App'
+import AssetDetail from './AssetDetail'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/asset/:assetId" element={<AssetDetail />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 )
